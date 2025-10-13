d8754f0bb16016ed5cf84a827c9adf273bca6a0f
BadiiiiX <contact@mehdi-ali.dev>
2025-10-13 21:46:10 +0200

    fix(core): Build result
    Signed-off-by: BadiiiX <contact@mehdi-ali.me>

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -2,17 +2,23 @@ import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-    plugins: [dts({ insertTypesEntry: true })],
+    plugins: [
+        dts({ insertTypesEntry: true })
+    ],
     build: {
         lib: {
             entry: 'src/index.ts',
             name: 'StrikeCore',
             formats: ['es', 'umd'],
-            fileName: (f) => (f === 'es' ? 'index.mjs' : 'index.umd.cjs'),
+            fileName: (format) => {
+                if (format === 'es') return 'index.mjs'
+                if (format === 'umd') return 'index.umd.cjs'
+            }
         },
         rollupOptions: {
-            external: []
+            external: [],
         },
-        sourcemap: true
+        sourcemap: true,
+        emptyOutDir: true,
     }
 })
